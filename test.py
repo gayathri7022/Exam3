fc132bf4a5b1a204accf2d8bc881b9e72b966c3f
gayathri7022 <gayathria7022@gmail.com>
2025-04-30 10:22:33 +0530

    Adding files

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from Attendence import register, view_attendence, mark_attendence, students
 students.clear()
 
 def test_cases():
-    assert register("Gayathri", "gayathri@gmail.com", "12345678") == "Email already exists"
+    assert register("Gayathri", "gayathri@gmail.com", "12345678") == "Registration successfull"
     assert mark_attendence("001", "gayathri@gmail.com", "30-04-2025") == "Attendence marked"
     result = view_attendence("001","gayathri@gmail.com" )
     assert result == "Attendence of 001 : ['30-04-2025']"
